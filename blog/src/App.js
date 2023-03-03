018d407914b10ecfcb94d0582ca1099ab09b65ec
bilguun082 <bilguune060829@gmail.com>
2023-03-03 19:38:42 +0800

    let's finish thissss

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,19 +1,28 @@
+import ThemeContext from "./context/ThemeContext";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "../src/pages/Home";
 import Login from "../src/pages/Login";
 import SignUp from "../src/pages/Sign";
-import BlogPost from "./pages/BlogPost";
+import Header from "./layout/Header";
+import Footer from "./layout/Footer";
+import { Box } from "@mui/system";
+
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/sign" element={<SignUp />}/>
-        <Route path="/blog" element={<BlogPost />} />
-      </Routes>
-    </BrowserRouter>
+    <ThemeContext>
+      <BrowserRouter>
+        <Box>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/sign" element={<SignUp />} />
+          </Routes>
+          <Footer />
+        </Box>
+      </BrowserRouter>
+    </ThemeContext>
   );
 }
 
