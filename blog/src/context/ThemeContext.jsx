22023d9bfcf637610688f1cc01c8ead5fbca1b64
bilguun082 <bilguune060829@gmail.com>
2023-03-13 19:40:49 +0800

    not finished

--- blog/src/context/ThemeContext.jsx
+++ blog/src/context/ThemeContext.jsx
@@ -4,13 +4,19 @@ export const ColorModeContext = createContext();
 
 export function ThemeContext({ children }) {
   const [theme, setTheme] = useState("dark");
+  const [username, setUsername] = useState("");
 
   const colorModeHander = () => {
     setTheme((prev) => (prev === "dark" ? "white" : "dark"));
   };
   return (
     <ColorModeContext.Provider
-      value={{ theme: theme, changeTheme: colorModeHander }}
+      value={{
+        theme: theme,
+        changeTheme: colorModeHander,
+        user: username,
+        setUser: setUsername,
+      }}
     >
       {children}
     </ColorModeContext.Provider>
